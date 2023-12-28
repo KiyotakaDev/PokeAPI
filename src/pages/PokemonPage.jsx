a83d36bc9bc8d945bd2ab37dce9720473f2daaf4
きよたか <c4mo.sw@gmail.com>
2023-12-28 00:16:54 -0500

    feat(PokemonPage.jsx): Component is created

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import usePokemonStore from '../store/pokemonStore'
 
 const PokemonPage = () => {
+  const { pokemonByID, fetchPokemonByID } = usePokemonStore()
   const { id } = useParams()
 
+  useEffect(() => {
+    return () => fetchPokemonByID(id)
+  }, [fetchPokemonByID, id])
+  
+
   return (
-    <div>
-      <h2>Welcome to the pokemon #{id} page</h2>
+    <div className='bg-pokemon-purple h-screen w-full text-white'>
+      {pokemonByID ? (
+        <div>
+          <h2>{pokemonByID.name}</h2>
+          <span>{pokemonByID.id}</span>
+          <img className='w-40 h-auto' src={pokemonByID.sprite} alt={pokemonByID.name} />
+          <div className=''>
+            {pokemonByID.types ? pokemonByID.types.map((type) => (
+              <span key={type.name} className={`${type.name} p-2 m-4 rounded-lg`}>{type.name}</span>
+            )): null}
+          </div>
+          {console.log(pokemonByID)}
+        </div>
+      ) : null}
       <Link to="/">Back to home page</Link>
     </div>
   )
