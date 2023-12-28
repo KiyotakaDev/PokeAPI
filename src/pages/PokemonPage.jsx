ae1218131ec5410884cc5eb08bf47774a812163e
きよたか <c4mo.sw@gmail.com>
2023-12-28 00:25:53 -0500

    test(production): Testing for production

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -14,19 +14,18 @@ const PokemonPage = () => {
   return (
     <div className='bg-pokemon-purple h-screen w-full text-white'>
       {pokemonByID ? (
-        <div>
+        <div className='flex justify-center items-center flex-col h-screen'>
           <h2>{pokemonByID.name}</h2>
           <span>{pokemonByID.id}</span>
-          <img className='w-40 h-auto' src={pokemonByID.sprite} alt={pokemonByID.name} />
+          <img className='w-1/2 h-auto mx-auto' src={pokemonByID.sprite} alt={pokemonByID.name} />
           <div className=''>
             {pokemonByID.types ? pokemonByID.types.map((type) => (
               <span key={type.name} className={`${type.name} p-2 m-4 rounded-lg`}>{type.name}</span>
             )): null}
           </div>
-          {/* {console.log(pokemonByID)} */}
+          <Link className='absolute bottom-10' to="/">Back to home page</Link>
         </div>
       ) : null}
-      <Link to="/">Back to home page</Link>
     </div>
   )
 }
