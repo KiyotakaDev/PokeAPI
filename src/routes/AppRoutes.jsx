76d40ba136183133caa775b87233960e1b25e1a7
きよたか <c4mo.sw@gmail.com>
2023-12-29 19:59:02 -0500

    feat(SearchPage): Search page is added to the routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import PokemonPage from '../pages/PokemonPage'
+import SearchPage from '../pages/SearchPage'
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path='/'>
         <Route index element={<HomePage />} />
         <Route path='pokemon/:id' element={<PokemonPage />} />
+        <Route path='search' element={<SearchPage />} />
 
         <Route path='*' element={<Navigate to="/" />} />
       </Route>
