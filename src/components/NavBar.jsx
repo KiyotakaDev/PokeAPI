a548568098cd6143bd87bf9673dc2b97747d1ed1
きよたか <c4mo.sw@gmail.com>
2023-12-29 20:33:31 -0500

    feat(Search): Navbar search is created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import usePokemonStore from "../store/pokemonStore";
 
 const NavBar = () => {
+  const { fetchBasePokemonByID, filterPokemons, searchTerm, setSearchTerm } =
+    usePokemonStore();
+
+  const handleSearch = () => {
+    const filteredPokemons = filterPokemons();
+    console.log("Filtered pokemons: ", filteredPokemons);
+  };
+
   return (
     <div className="absolute flex justify-evenly items-center w-full py-5 px-8 transform transition duration-500">
-      <img className="h-auto w-[25%]" src="/pokeapi-logo.png" />
-      <Link
-        to="search"
-        children={
-          <p className="hover:text-yellow-300 transition-colors duration-200 animate-less_bounce">
-            PokeSearch
-          </p>
-        }
-      />
+      <div className="w-1/2">
+        <img className="h-auto w-1/2" src="/pokeapi-logo.png" />
+      </div>
+
+      <form
+        className="w-1/2 flex justify-between items-center border-2 border-pokemon-purple-50 rounded-lg py-2 px-4 gap-x-6"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full bg-transparent outline-none"
+        />
+        <button
+          className="hover:text-pokemon-purple-50 transition-colors duration-200"
+          onClick={handleSearch}
+          children={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>
+          }
+        />
+      </form>
     </div>
   );
 };
