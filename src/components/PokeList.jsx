349584dc4cd832bd2c72823729cd37d4ec4a4fbe
きよたか <c4mo.sw@gmail.com>
2023-12-29 19:58:34 -0500

    feat(PokeList): Styles are adjusted and NavBar component is used

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 import usePokemonStore from "../store/pokemonStore";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const styles = {
   container:
     "absolute bottom-0 xl:left-1/2 h-1/2 w-full xl:h-screen xl:w-1/2 overflow-y-scroll overflow-x-hidden p-4 xl:px-12 xl:py-6 flex flex-col gap-y-4 sm:gap-y-7 lg:gap-y-10 1k:gap-y-9 4k:gap-y-20",
   pokemonCard:
-    "relative bg-pokemon-purple-200 py-4 sm:py-7 lg:py-10 xl:py-8 2k:py-12 4k:py-16 rounded-br-full text-white text-shadow text-sm s:text-base ls:text-xl sm:text-2xl lg:text-4xl xl:text-2xl 1k:text-4xl 2k:text-5xl 4k:text-7xl",
+    "relative bg-pokemon-purple-200 py-4 sm:py-7 lg:py-10 xl:py-8 2k:py-12 4k:py-16 rounded-br-full",
+  pokeText:
+    "text-white text-shadow text-sm s:text-base ls:text-xl sm:text-2xl lg:text-4xl xl:text-2xl 1k:text-4xl 2k:text-5xl 4k:text-7xl",
   pokemonImage:
     "absolute h-16 w-16 ls:h-20 ls:w-20 sm:h-32 sm:w-32 lg:h-40 lg:w-40 xl:h-28 xl:w-28 1k:h-36 1k:w-36 2k:h-48 2k:w-48 4k:h-72 4k:w-72 -top-3 1k:-top-6 4k:-top-10 -left-3 xl:-left-8 4k:-left-12 object-contain",
   pokemonDetails:
@@ -81,17 +84,25 @@ const PokeList = () => {
   }, [pokemons]);
 
   return (
-    <>
-      <div className="w-full h-1/2 xl:w-1/2 xl:h-screen flex justify-center items-center">
-        {firstVisiblePokemonId && (
-          <Link to={`pokemon/${firstVisiblePokemonId.attributes.id.value}`}>
-            <img
-              className="object-cover max-w-full h-auto animate-less_bounce"
-              src={firstVisiblePokemonId.firstChild.src}
-              alt={firstVisiblePokemonId.firstChild.attributes.alt.value}
-            />
-          </Link>
-        )}
+    <div
+      className={`relative h-screen w-full flex flex-col xl:flex-row ${styles.pokeText}`}
+    >
+      <div className="absolute h-1/2 w-full xl:h-screen xl:w-1/2 ">
+        <NavBar />
+        <div className="w-full h-full flex justify-center items-end xl:items-center">
+          {firstVisiblePokemonId && (
+            <Link
+              className="flex justify-center items-center"
+              to={`pokemon/${firstVisiblePokemonId.attributes.id.value}`}
+            >
+              <img
+                className="object-cover w-[75%] h-auto animate-less_bounce"
+                src={firstVisiblePokemonId.firstChild.src}
+                alt={firstVisiblePokemonId.firstChild.attributes.alt.value}
+              />
+            </Link>
+          )}
+        </div>
       </div>
       {/* {firstVisiblePokemonId && console.log(firstVisiblePokemonId.attributes.id)} */}
       <div ref={containerRef} className={styles.container}>
@@ -101,7 +112,7 @@ const PokeList = () => {
             ))
           : null}
       </div>
-    </>
+    </div>
   );
 };
 
