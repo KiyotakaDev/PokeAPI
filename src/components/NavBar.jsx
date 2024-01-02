0f5e72807314a3b88c0becd6bf6b082d217863ba
きよたか <c4mo.sw@gmail.com>
2024-01-02 14:43:50 -0500

    ): Adding responsive design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,21 +26,12 @@ const NavBar = () => {
             setSearchTerm(e.target.value)
             filterPokemons()
           }}
-          className="w-full bg-transparent outline-none text-xs ls:text-sm sm:text-2xl lg:text-4xl xl:text-2xl 1k:text-4xl 2k:text-5xl 4k:text-7xl"
+          className="w-[90%] bg-transparent outline-none text-xs ls:text-sm sm:text-2xl lg:text-4xl xl:text-2xl 1k:text-4xl 2k:text-5xl 4k:text-7xl"
         />
         <button
-          className="hover:text-pokemon-purple-50 transition-colors duration-200"
+          className="hover:text-pokemon-purple-50 transition-colors duration-200 w-[10%]"
           children={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="currentColor"
-              className="bi bi-search"
-              viewBox="0 0 16 16"
-            >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
+            <img className="h-auto w-full" src="/search.svg" />
           }
         />
       </div>
