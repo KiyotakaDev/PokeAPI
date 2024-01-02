2a11697ac9b84deeb4942521fac4f5aaef0ea11f
きよたか <c4mo.sw@gmail.com>
2024-01-01 21:44:34 -0500

    style(loader): Chaning font size

--- src/components/PokeList.jsx
+++ src/components/PokeList.jsx
@@ -23,7 +23,7 @@ const Loader = () => {
   return (
     <div className="absolute h-full w-full flex justify-center items-center">
       <div className="flex flex-col gap-y-10 justify-center items-center">
-        <p className="animate-bounce">Catching Pokemons </p>
+        <p className="animate-bounce text-2xl xl:text-4xl 1k:text-5xl 2k:text-7xl 4k:text-9xl px-10 text-center">Catching Pokemons </p>
         <l-newtons-cradle
           size="120"
           speed="1.4"
