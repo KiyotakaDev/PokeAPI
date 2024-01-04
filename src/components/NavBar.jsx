f35e1e956363e75126ddcab9dc131d1c2039d853
きよたか <c4mo.sw@gmail.com>
2024-01-03 22:31:38 -0500

    feat(NavBar.jsx): Search term and selected card clean up

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import usePokemonStore from "../store/pokemonStore";
-import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  const { filterPokemons, searchTerm, setSearchTerm } = usePokemonStore();
-  const navigate = useNavigate()
+  const { filterPokemons, searchTerm, setSearchTerm, setSelectedCard } =
+    usePokemonStore();
 
   return (
     <div className="absolute flex justify-evenly items-center w-full py-5 px-8 gap-x-10 transform transition duration-500">
       <div className="w-[40%]">
-        <Link
-          to="/"
-          children={<img className="h-auto w-full transition-transform ease-out duration-200 hover:scale-105 active:scale-90" src="/pokeapi-logo.png" />}
+        <img
+          className="h-auto w-full transition-transform ease-out duration-200 hover:scale-105 active:scale-90"
+          src="/pokeapi-logo.png"
+          onClick={() => {
+            setSearchTerm("");
+            setSelectedCard(null);
+          }}
         />
       </div>
 
-      <div
-        className="w-[60%] flex justify-between items-center border-2 border-pokemon-purple-50 rounded-lg py-2 px-4 gap-x-2"
-      >
+      <div className="w-[60%] flex justify-between items-center border-2 border-pokemon-purple-50 rounded-lg py-2 px-4 gap-x-2">
         <input
           type="text"
           placeholder="Search"
           value={searchTerm}
           onChange={(e) => {
-            setSearchTerm(e.target.value)
-            filterPokemons()
+            setSearchTerm(e.target.value);
+            filterPokemons();
           }}
           className="w-[91%] xl:w-[90%] bg-transparent outline-none text-xs ls:text-sm sm:text-2xl lg:text-4xl xl:text-2xl 1k:text-4xl 2k:text-5xl 4k:text-7xl"
         />
